d76cad6f519829fafb38083f3b2a30be3d5490e4
kevinassobo <kevinbagueka@yahoo.fr>
2022-01-14 02:55:42 +0100

    build authors graphs

--- code/baseline/graph_text_baseline.py
+++ code/baseline/graph_text_baseline.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 from random import randint
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 from gensim.models.doc2vec import Doc2Vec
 
@@ -21,12 +22,26 @@ with open('data/initial_data/abstracts.txt', 'r') as f:
         node, abstract = line.split('|--|')
         abstracts[int(node)] = abstract
 
+# Read the authors to each paper
+authors = dict()
+with open('data/processed_data/authors_ids.txt', 'r') as f:
+    for line in f:
+        node, node_authors = line.rstrip('\n').split('|--|')
+        authors[int(node)] = node_authors.split(',')
+
 # Read the Doc2vec model
 doc2vec_model = Doc2Vec.load('data/models/doc2vec_dm_64.model')
 
 # Map text to set of terms
 for node in abstracts:
     abstracts[node] = set(abstracts[node].split())
+
+# Read test data. Each sample is a pair of nodes
+node_pairs = list()
+with open('data/initial_data/test.txt', 'r') as f:
+    for line in f:
+        t = line.split(',')
+        node_pairs.append((int(t[0]), int(t[1])))
 
 # Create the training matrix. Each row corresponds to a pair of nodes and
 # its class label is 1 if it corresponds to an edge and 0, otherwise.
@@ -37,7 +52,7 @@ for node in abstracts:
 # (2) absolute value of difference of number of unique terms of the two nodes' abstracts
 # (3) number of common terms between the abstracts of the two nodes
 
-X_train = np.zeros((2*m, 6))
+X_train = np.zeros((2*m, 7))
 y_train = np.zeros(2*m)
 for i,edge in enumerate(G.edges()):
     # an edge
@@ -47,6 +62,7 @@ for i,edge in enumerate(G.edges()):
     X_train[2*i,3] = abs(len(abstracts[edge[0]]) - len(abstracts[edge[1]]))
     X_train[2*i,4] = len(abstracts[edge[0]].intersection(abstracts[edge[1]]))
     X_train[2*i,5] = doc2vec_model.docvecs.similarity(edge[0], edge[1])
+    X_train[2*i,6] = len(set(authors[edge[0]]) & set(authors[edge[1]]))
     y_train[2*i] = 1
 
     # a randomly generated pair of nodes
@@ -58,19 +74,15 @@ for i,edge in enumerate(G.edges()):
     X_train[2*i+1,3] = abs(len(abstracts[n1]) - len(abstracts[n2]))
     X_train[2*i+1,4] = len(abstracts[n1].intersection(abstracts[n2]))
     X_train[2*i+1,5] = doc2vec_model.docvecs.similarity(n1, n2)
+    X_train[2*i+1,6] = len(set(authors[n1]) & set(authors[n2]))
     y_train[2*i+1] = 0
 
 print('Size of training matrix:', X_train.shape)
 
-# Read test data. Each sample is a pair of nodes
-node_pairs = list()
-with open('data/initial_data/test.txt', 'r') as f:
-    for line in f:
-        t = line.split(',')
-        node_pairs.append((int(t[0]), int(t[1])))
+
 
 # Create the test matrix. Use the same 2 features as above
-X_test = np.zeros((len(node_pairs), 6))
+X_test = np.zeros((len(node_pairs), 7))
 for i,node_pair in enumerate(node_pairs):
     X_test[i,0] = G.degree(node_pair[0]) + G.degree(node_pair[1])
     X_test[i,1] = abs(G.degree(node_pair[0]) - G.degree(node_pair[1]))
@@ -78,10 +90,15 @@ for i,node_pair in enumerate(node_pairs):
     X_test[i,3] = abs(len(abstracts[node_pair[0]]) - len(abstracts[node_pair[1]]))
     X_test[i,4] = len(abstracts[node_pair[0]].intersection(abstracts[node_pair[1]]))
     X_test[i,5] = doc2vec_model.docvecs.similarity(node_pair[0], node_pair[1])
+    X_test[i,6] = len(set(authors[node_pair[0]]) & set(authors[node_pair[1]]))
 
 print('Size of test matrix:', X_test.shape)
 
 # Use logistic regression to predict if two nodes are linked by an edge
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 clf = LogisticRegression()
 clf.fit(X_train, y_train)
 y_pred = clf.predict_proba(X_test)
@@ -94,3 +111,4 @@ with open("submission.csv","w") as pred:
     csv_out.writerow(['id','predicted'])
     for row in predictions:
         csv_out.writerow(row) 
+
